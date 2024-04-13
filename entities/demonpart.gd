9fee4482cbc193fba2488ff416a26810ddb575df
cnj3565 <cjensen6050@gmail.com>
2024-04-13 14:39:56 -0400

    part generation method

--- entities/demonpart.gd
+++ entities/demonpart.gd
@@ -3,9 +3,11 @@ extends Node2D
 @export var part_type : String = "body"
 @export var demon_type : String = "devil"
 
+@onready var animator : AnimatedSprite2D = $Sprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	createPart(2, 3)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,25 +19,35 @@ func createPart(type = 0, part = 0):
 	match part:
 		0:
 			part_type = "body"
+			animator.play("bodies")
 		1:
 			part_type = "head"
+			animator.play("heads")
 		2:
 			part_type = "arm_l"
+			animator.play("arms_l")
 		3:
 			part_type = "arm_r"
+			animator.play("arms_r")
 		4:
 			part_type = "leg_l"
+			animator.play("legs_l")
 		5:
 			part_type = "leg_r"
+			animator.play("legs_r")
 	match type:
 		0:
 			demon_type = "devil"
+			animator.frame = 0
 		1:
 			demon_type = "bird"
+			animator.frame = 1
 		2:
 			demon_type = "horror"
+			animator.frame = 2
 		3:
 			demon_type = "crab"
-
-	
-	
+			animator.frame = 3
+	print(demon_type, ", ", part_type)
+	animator.pause()
+	print("Frame: ", animator.frame)
