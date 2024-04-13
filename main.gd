f003a9781077e2690f63a039640375ebc38fe1ec
cnj3565 <cjensen6050@gmail.com>
2024-04-13 16:30:25 -0400

    randomly placed generation code done

--- main.gd
+++ main.gd
@@ -4,12 +4,20 @@ var parts_spawned : Array = []
 var parts_collected : Array = []
 var demon_part = preload("res://entities/demonpart.tscn")
 
+var spawns : Array = []
+var spawn_taken : Array = []
+
 @onready var parts_holder : Node2D = $Parts
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# spawner setup
+	spawns = $Spawns.get_children()
+	for r in range(0,spawns.size()):
+		spawn_taken.append(false)
+	
 	spawn_parts()
-
+	print("Taken Array: ", spawn_taken)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,18 +31,28 @@ func spawn_parts():
 		parts_spawned.append(newpart)
 		
 		# set to markers later
-		newpart.position = $Player.position + Vector2(64,-10)
+		var spawn_picker = randi_range(0,spawns.size()-1)
+		while spawn_taken[spawn_picker] == true:
+			spawn_picker = randi_range(0,spawns.size()-1)
+		
+		newpart.position = spawns[spawn_picker].position
+		spawn_taken[spawn_picker] = true
 		newpart.createPart(Global.prompt, r)
 	
 	# generates random amount of extra parts
-	var parts_amount = randi_range(6, 14)
+	var parts_amount = randi_range(8, 14)
 	for a in range(0,parts_amount):
 		var extrapart = demon_part.instantiate()
 		parts_holder.add_child(extrapart)
 		parts_spawned.append(extrapart)
 		
 		# set to markers later
-		extrapart.position = $Player.position + Vector2(64,-10)
+		var spawn_picker = randi_range(0,spawns.size()-1)
+		while spawn_taken[spawn_picker] == true:
+			spawn_picker = randi_range(0,spawns.size()-1)
+		
+		extrapart.position = spawns[spawn_picker].position
+		spawn_taken[spawn_picker] = true
 		
 		var rand_demon = Global.prompt
 		while rand_demon == Global.prompt:
