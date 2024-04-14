742905a679ed4cf3f035111c333cacf29d2bd53a
cnj3565 <cjensen6050@gmail.com>
2024-04-14 18:25:53 -0400

    added lava

---
+++ world/lava.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var player: Node2D = null
+@onready var sprite: Node2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	sprite.play("default")
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	player.velocity.y = -player.jump_velocity * 10
+	player.hurt_sound.play()
+
+func _on_area_2d_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	pass # Replace with function body.
