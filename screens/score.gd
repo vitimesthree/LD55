9a1ec368b102c7150bc142761abdd290327af7f4
cnj3565 <cjensen6050@gmail.com>
2024-04-14 16:32:10 -0400

    basic scoring system

--- screens/score.gd
+++ screens/score.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+@onready var smarts : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	calculate_score()
+	
+	$Score.text = str(smarts)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -13,3 +15,22 @@ func _process(_delta):
 func _input(event):
 	if event.is_action_pressed("jump"):
 		get_tree().change_scene_to_file('res://main.gd')
+
+func calculate_score():
+	var prompt = Global.required_demon
+	var collected = Global.parts_collected
+	
+	for r in range(0,6):
+		var current = collected[r]
+		if current[1] == prompt:
+			smarts += 100
+		else:
+			smarts += 50
+	
+	if(smarts == 6000):
+		smarts = 999999
+		$Perfect.visible = true
+	elif(smarts >= 3500):
+		$Good.visible = true
+	else:
+		$Fail.visible = true
