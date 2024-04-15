3d524fa28c2912defeb69715fd5db85a864c766a
cnj3565 <cjensen6050@gmail.com>
2024-04-14 20:26:15 -0400

    added scoring table

--- screens/score.gd
+++ screens/score.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @onready var smarts : int = 0
 @onready var next_scene : String = "res://screens/tutorial.tscn"
+@onready var score_table : Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	make_score_table()
 	$Clue.text = Global.clue
 	
 	calculate_score()
@@ -19,15 +21,30 @@ func _input(event):
 		get_tree().change_scene_to_file(next_scene)
 
 func calculate_score():
-	var prompt = Global.required_demon
+	var prompt = Global.prompt
 	var collected = Global.parts_collected
+	
+	# Gets the index number for which Demon is the Best
+	var prompt_index = score_table[prompt]
 	
 	for r in range(0,6):
 		var current = collected[r]
-		if current[1] == prompt:
-			smarts += 1000
-		else:
-			smarts += 500
+		
+		# Gets the demon type of the current part
+		var which_demon = current[1]
+		var demon_index = 0
+		
+		match which_demon:
+			"devil": demon_index = 0
+			"bird": demon_index = 1
+			"horror": demon_index = 2
+			"crab": demon_index = 3
+		
+		# Adds score based on how much the current demon type
+		# is worth, based on the tutorial clue
+		var add_score = prompt_index[demon_index]
+		smarts += add_score
+		print(smarts)
 
 	if(smarts == 6000):
 		smarts = 999999
@@ -39,3 +56,16 @@ func calculate_score():
 	else:
 		$Fail.visible = true
 		next_scene = "res://screens/bad_ending.tscn"
+
+func make_score_table():
+	# Prompt: Devil
+	score_table.append([1000, 200, 450, 100])
+	
+	# Prompt: Bird
+	score_table.append([50, 1000, 200, 650])
+	
+	# Prompt: Horror
+	score_table.append([400, 250, 1000, 100])
+	
+	# Prompt: Crab
+	score_table.append([100, 350, 500, 1000])
