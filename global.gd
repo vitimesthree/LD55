49596e6874b1ae33c77650047d7a598f4f90f561
vitimesthree <120028754+vitimesthree@users.noreply.github.com>
2024-04-15 21:03:07 +0200

    Add window sizing and fullscreen toggle

--- global.gd
+++ global.gd
@@ -6,6 +6,11 @@ var required_demon: String = ''
 var score : int = 0
 var demon_hex = "ffffff"
 
+@onready var window_size: Vector2 = get_window().size
+var window_scale: int = 1
+const max_scale: int = 4
+var is_fullscreen: bool = false
+
 # Parts stored as mini-arrays [0,1]
 # 0 is the part type (head, body, etc)
 # 1 is the demon type (devil, bird, etc)
@@ -26,5 +31,16 @@ func _process(_delta):
 func _input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
+	if event.is_action_pressed("window_size"):
+		if window_scale < max_scale:
+			window_scale = window_scale + 1
+		else:
+			window_scale = 1
+		get_window().size = window_size * window_scale
 	if event.is_action_pressed("fullscreen"):
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		if is_fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			is_fullscreen = false
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+			is_fullscreen = true
