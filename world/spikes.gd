dc2132586ebff7d85dcf2a82410f2e281ae82835
vitimesthree <120028754+vitimesthree@users.noreply.github.com>
2024-04-15 20:05:47 +0200

    Fix delta warnings

--- world/spikes.gd
+++ world/spikes.gd
@@ -7,7 +7,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
